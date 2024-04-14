32e0caaf8eb684bd5b057275279b2c30133d62e6
Joyce <joyce.saldanha1999@gmail.com>
2024-04-14 21:28:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { GithubService } from './Services/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'fyle';
+  constructor(
+    private apiService: GithubService
+  ) {}
+
+  ngOnInit() {
+    this.apiService.getProfile('johnpapa').subscribe(console.log);
+  }
 }
